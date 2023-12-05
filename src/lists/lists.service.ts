446725ebdc958249e54f17c3075c8ab355467335
Leobritt <leobritto160@gmail.com>
2023-12-05 15:10:02 -0300

    finalizando o CRUD

--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -1,26 +1,54 @@
 import { Injectable } from '@nestjs/common';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
+import { List } from './entities/list.entity';
+import { InjectModel } from '@nestjs/sequelize';
+import { error } from 'console';
 
 @Injectable()
 export class ListsService {
+
+  constructor(
+   //instancia que vai tá no bd e tipando com o typeof do TS
+   //usando o decorator para injetar a instencia passando como param o tipo do dado
+   @InjectModel(List)
+   private listModel: typeof List
+  ){
+  };
   create(createListDto: CreateListDto) {
-    return 'This action adds a new list';
+  return this.listModel.create(createListDto);
   }
 
   findAll() {
-    return `This action returns all lists`;
+    return this.listModel.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} list`;
+ async findOne(id: number) {
+    const list = await this.listModel.findByPk(id);
+    if(!list){
+    throw new Error('List not found');
+    }
+    return list;
   }
 
-  update(id: number, updateListDto: UpdateListDto) {
-    return `This action updates a #${id} list`;
+  async update(id: number, updateListDto: UpdateListDto) {
+  const list = await this.listModel.findByPk(id);
+
+  if(!list){
+  throw new Error('List not found');
+  }
+  await list.update(updateListDto)
+    return list;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} list`;
+  async remove(id: number) {
+  const list = await this.listModel.findByPk(id);
+
+  if (!list) {
+    throw new Error('List not found');
   }
+
+  await list.destroy(); // Use o método destroy na instância específica do modelo
+}
+
 }
