7b670d5166f621cb33e73d7a8c9c580692f26cdb
Leobritt <leobritto160@gmail.com>
2023-12-06 11:36:57 -0300

    correção do path no list.service deixando igual ao do api.http

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -3,10 +3,15 @@ import { ListsService } from './lists.service';
 import { ListsController } from './lists.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { List } from './entities/list.entity';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
-  SequelizeModule.forFeature([List])
+  SequelizeModule.forFeature([List]),
+  //após colocar o axios colcoar no module a info nesse caso path fake-api do packge.json
+  HttpModule.register({
+  baseURL: 'http://localhost:8000',
+  }),
   ],
   controllers: [ListsController],
   providers: [ListsService],
--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -4,51 +4,62 @@ import { UpdateListDto } from './dto/update-list.dto';
 import { List } from './entities/list.entity';
 import { InjectModel } from '@nestjs/sequelize';
 import { error } from 'console';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
 export class ListsService {
 
+  //instancia que vai tá no bd e tipando com o typeof do TS  
+  //usando o decorator para injetar a instencia passando como param o tipo do dado
   constructor(
-   //instancia que vai tá no bd e tipando com o typeof do TS
-   //usando o decorator para injetar a instencia passando como param o tipo do dado
-   @InjectModel(List)
-   private listModel: typeof List
-  ){
-  };
-  create(createListDto: CreateListDto) {
-  return this.listModel.create(createListDto);
+    @InjectModel(List)
+    private listModel: typeof List,
+    //vem do axios 
+    private httpService: HttpService,
+  ) {};
+  async create(createListDto: CreateListDto) {
+    //usando o axios alem de criar vou precisar fazer o envio via http para crm externo
+    const list = await this.listModel.create(createListDto);
+    //post pq será inserido no crm uasndo o lastValueFrom para trasnforma em uma promisse
+    await lastValueFrom(
+    this.httpService.post('lists',{
+    name: list.name,
+    }));
+    //regra de néocio cria no banco e depois cria na lista
+    return list;
   }
 
   findAll() {
     return this.listModel.findAll();
   }
 
- async findOne(id: number) {
+  async findOne(id: number) {
     const list = await this.listModel.findByPk(id);
-    if(!list){
-    throw new Error('List not found');
+    if (!list) {
+      throw new Error('List not found');
     }
     return list;
   }
 
   async update(id: number, updateListDto: UpdateListDto) {
-  const list = await this.listModel.findByPk(id);
+    const list = await this.listModel.findByPk(id);
 
-  if(!list){
-  throw new Error('List not found');
-  }
-  await list.update(updateListDto)
+    if (!list) {
+      throw new Error('List not found');
+    }
+    await list.update(updateListDto)
     return list;
   }
 
   async remove(id: number) {
-  const list = await this.listModel.findByPk(id);
+    const list = await this.listModel.findByPk(id);
 
-  if (!list) {
-    throw new Error('List not found');
+    if (!list) {
+      throw new Error('List not found');
+    }
+
+    await list.destroy(); // Use o método destroy na instância específica do modelo
   }
-
-  await list.destroy(); // Use o método destroy na instância específica do modelo
-}
 
 }
